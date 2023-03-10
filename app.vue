2749e334c47a0abe61f943a438f586d8f51b9e76
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-10 20:51:10 +0700

    ADD FONTS MONSERRAT

--- app.vue
+++ app.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
-    <!-- <NuxtWelcome /> -->
-    <div class="font-mono font-extrabold text-gray-800 text-5xl mx-auto mt-36 text-center leading-tight">Future Living -
-      Team
-      3</div>
+   <NuxtLayout>
+    <NuxtPage/>
+   </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="font-montserrat font-extrabold text-2xl text-teal-800">Bismillah</div>
+</template>
