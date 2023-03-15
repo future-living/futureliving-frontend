49d018d34dc21ddb96134c1db127b5cbb582eec7
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2023-03-16 02:23:18 +0700

    ADD Motto & Vision Section on About Page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="base-section-container">
-        <Spacer />
-        <About />
-        <Spacer />
-    </div>
+  <Spacer />
+  <div class="base-section-container">
+    <About />
+  </div>
+  <Spacer />
+  <div class="base-section-container bg-[#EDE9EF]">
+    <AboutMotovision class="" />
+  </div>
+  <Spacer />
 </template>
