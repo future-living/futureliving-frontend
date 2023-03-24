c98ec08a68825454f3b4fb71dd92054e1d3645ef
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-25 06:10:13 +0700

    ADD HOME PAGE SEO

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,26 @@
+<script setup>
+
+useHead({
+    title: "Future Living",
+    meta: [
+        { name: 'description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'keywords', content: 'future, living, home, house, buy, sell' },
+        { name: 'og:image', content: 'https://future-living.netlify.app/future-living-logo.png' },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://future-living.netlify.app' },
+        { name: 'og:title', content: 'Future Living' },
+        { name: 'og:description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://future-living.netlify.app' },
+        { name: 'twitter:title', content: 'Future Living' },
+        { name: 'twitter:description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'twitter:image', content: 'https://future-living.netlify.app/future-living-logo.png' },
+
+    ],
+})
+
+</script>
+
 <template>
     <div class="base-section-container pt-4 md:pt-8 lg:pt-16">
         <HomeHero />
