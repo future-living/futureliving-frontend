00ffe57f94b92f9b9e0160fe3904f67c942fcabf
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2023-03-25 16:01:17 +0700

    ADD section JAJARAN DIREKSI

---
+++ components/about/direction/card.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    nama: String,
+    jabatan: String,
+    img: String,
+})
+</script>
+<template>
+    <div class=" flex flex-col items-center gap-6 md:gap-16 lg:gap-20 pb-10">
+        <img class="object-cover h-[364px] w-[310px] rounded-lg" :src="img" alt="">
+        <div class="absolute bottom-0 rounded-lg p-4 bg-white items-center flex flex-col min-w-[182px]">
+            <div class="font-bold font-montserrat text-base text-title">{{ nama }}</div>
+            <div class="font-montserrat text-sm text-[#858D9D]">{{ jabatan }}</div>
+        </div>
+    </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -8,8 +8,12 @@
     <AboutMotovision class="" />
   </div>
   <Spacer />
-  <div class="base-section-container bg-[#EDE9EF]">
+  <div class="base-section-container">
     <AboutSejarah />
+  </div>
+  <Spacer />
+  <div class="project-section-container bg-[#EDE9EF]">
+    <AboutDirection />
   </div>
   <Spacer />
 </template>
