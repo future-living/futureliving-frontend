983aeb27f19a82b2cc57784c620e281a64aba9d1
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2023-03-28 16:31:00 +0700

    ADD section about property and residence on detail page

--- components/detail/property/index.vue
+++ components/detail/property/index.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        ABOUT PROPERTY SECTION
+    <div class="flex w-full flex-col gap-6 lg:w-fit lg:flex-row lg:justify-between lg:gap-16 py-[70px]">
+
+        <div class="flex flex-col items-center justify-center gap-4 md:gap-7 lg:items-start lg:gap-10 columns-6">
+            <div
+                class="text-center font-montserrat text-2xl font-bold text-title md:text-[44px] md:leading-[54px] lg:max-w-[568px] lg:text-left lg:leading-[73px]">
+                Tentang Properti Ini
+            </div>
+            <div
+                class="md:font-reguler text-center font-montserrat text-xs font-light leading-6 text-bodyText md:text-lg md:leading-[30px] lg:max-w-[568px] lg:text-left">
+                Telah menjadi kebanggaan dan pilihan setiap keluarga muda selama 10 tahun Future Living berkarya. Benefit
+                yang didapatkan tidak hanya dari fasilitas namun nilai investasi yang menjanjikan. Kenyamanan Anda adalah
+                prioritas FutureLiving dan dengan teknologi terkini rasakan kenyamanan dari Hunian Lavenia.
+            </div>
+        </div>
+        <div class=" relative flex flex-col items-center gap-3 md:gap-6 md:px-[52px] w-full lg:w-fit lg:px-0 px-7">
+            <div
+                class="text-center font-montserrat text-2xl font-bold text-title md:text-[44px] md:leading-[54px] lg:max-w-[568px] lg:text-left lg:leading-[73px]">
+                Denah Rumah
+            </div>
+            <img src="~/assets/img/denah.png" alt="" />
+        </div>
     </div>
 </template>
+  
--- components/detail/residence/index.vue
+++ components/detail/residence/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        ABOUT RESIDENCE SECTION
+    <div class="flex w-full flex-col gap-6 lg:w-fit lg:flex-row lg:justify-between lg:gap-16">
+        <div class=" relative flex flex-row gap-3 md:gap-6 md:px-[52px] w-full lg:w-fit lg:px-0 px-7">
+            <img src="~/assets/img/about_residence.png" alt="" />
+        </div>
+        <div class="flex flex-col items-center justify-center gap-4 md:gap-7 lg:items-start lg:gap-10 columns-6">
+            <div
+                class="text-center font-montserrat text-2xl font-bold text-title md:text-[44px] md:leading-[54px] lg:max-w-[568px] lg:text-left lg:leading-[73px]">
+                Tentang Type Lavenia
+            </div>
+            <div
+                class="md:font-reguler text-center font-montserrat text-xs font-light leading-6 text-bodyText md:text-lg md:leading-[30px] lg:max-w-[568px] lg:text-left">
+                Lavenia merupakan hunian Eco-Friendly dengan sirkulasi udara dan pencahayaan yang baik. Hunian idaman
+                keluarga muda yang hemat energi dapat meningkatkan kualitas hidup Anda dan keluarga. Tipe Lavenia dengan
+                konsep yang modern dan minimalis dapat menjadi pilihan Anda dan keluarga untuk berinvestasi jangka panjang.
+            </div>
+        </div>
     </div>
 </template>
+  
