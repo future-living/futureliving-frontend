06798796a3c95b70990af494196741c2ea5b6382
MBrian Website <muhammad.brian.27@gmail.com>
2023-03-28 23:19:11 +0700

    ADD SCROLL TO TOP

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <Navbar />
-  <NuxtPage />
-  <Footer />
+  <div class="relative">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <NuxtLink to="#top" class="fixed bottom-5 right-5">
+      <div class="bg-[#F3F1F4] p-[12px] bg-opacity-80">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="w-[24px] h-[24px] stroke-title stroke-[3px]">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+        </svg>
+      </div>
+    </NuxtLink>
+  </div>
 </template>
