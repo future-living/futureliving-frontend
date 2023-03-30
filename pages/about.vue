8f6d9be6d1e2dcb9235c9fdbb4c246a49a962443
Aghniya Ni'amillah Nurhilman <aghniyan@gmail.com>
2023-03-30 14:58:07 +0700

    ADD SEO and response button video and 360

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,24 @@
+<script setup>
+
+useHead({
+    title: "About - Future Living",
+    meta: [
+        { name: 'description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'keywords', content: 'future, living, home, house, buy, sell' },
+        { name: 'og:image', content: 'https://future-living.netlify.app/future-living-logo.png' },
+        { name: 'og:type', content: 'website' },
+        { name: 'og:url', content: 'https://future-living.netlify.app/about' },
+        { name: 'og:title', content: 'About - Future Living' },
+        { name: 'og:description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:url', content: 'https://future-living.netlify.app/about' },
+        { name: 'twitter:title', content: 'About - Future Living' },
+        { name: 'twitter:description', content: 'Find The Place To Live Your Dreams Easily Here' },
+        { name: 'twitter:image', content: 'https://future-living.netlify.app/future-living-logo.png' },
+    ],
+})
+
+</script>
 <template>
   <Spacer />
   <div class="base-section-container">
